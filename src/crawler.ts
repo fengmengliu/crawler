d2f6b1aa5c7d586c829bc6b17c305f119cf694aa
fengmengliu <liufengmeng@youxin.com>
2020-08-12 22:43:58 +0800

    文件的读取与新数据的写入

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import superagent from 'superagent';
 import cheerio from 'cheerio';
 
@@ -5,9 +7,19 @@ interface Course{
   desc: string
 }
 
+interface CourseInfo{
+  time: number,
+  data: Course[]
+}
+
+interface Content{
+  [propName: number]: Course[]
+}
+
 class Crawler {
   private secret= 'x3b174jsx';
   private url = `http://www.dell-lee.com/typescript/demo.html + ${this.secret}`;
+  private filepath = path.resolve(__dirname, '../data/content.json');
 
   getCourseInfo(html: string) {
     const $ = cheerio.load(html); // 将html字符串传递给load方法，类似于获取到了jquery对象；之后就是和jquery一样的操作了
@@ -28,9 +40,25 @@ class Crawler {
     return result.text;
   };
 
+  getJsonData(courseResult: CourseInfo) {
+    let fileContent: Content = {};
+    if(fs.existsSync(this.filepath)){ // 取出文件中存储的数据
+      fileContent = JSON.parse(fs.readFileSync(this.filepath, 'utf-8'));
+    }
+    // 添加新的数据
+    fileContent[courseResult.time] = courseResult.data;
+    return fileContent;
+  };
+
+  writeFile(content: string) {
+    fs.writeFileSync(this.filepath, content);
+  }
+
   async initCrawler() {
     const html = await this.getHtml();
-    const courseResult = this.getCourseInfo(html);
+    const courseResult: CourseInfo = this.getCourseInfo(html);
+    const fileContent = this.getJsonData(courseResult);
+    this.writeFile(JSON.stringify(fileContent));
   }
 
   constructor(){
